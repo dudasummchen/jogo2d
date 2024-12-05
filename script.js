15864f3bf4bad5db2d916a656a3bd6cb572d34f4
maria eduarda summchen <04595523092@senacrs.edu.br>
2024-12-05 10:10:54 -0300

    jogo atualizado

--- script.js
+++ script.js
@@ -12,16 +12,13 @@ const reposicionarTubos = () => {
   tubos.forEach(tubo => {
     const tuboPosition = tubo.offsetLeft;
     
-    // Se o tubo saiu da tela, reposiciona
-    if (tuboPosition <= -60) {
-      // Adiciona uma distância aleatória entre os tubos
-      const distanciaMinima = 200; // Distância mínima entre os tubos
-      const distanciaMaxima = 300; // Distância máxima entre os tubos
+
+    if (tuboPosition <= -60) { //distância dos tubos
+      const distanciaMinima = 200; // distância mínima entre os tubos
+      const distanciaMaxima = 300; // distância máxima entre os tubos
       const novaPosicao = window.innerWidth + Math.floor(Math.random() * (distanciaMaxima - distanciaMinima) + distanciaMinima);
       tubo.style.left = `${novaPosicao}px`;
 
-      // Adiciona uma altura aleatória para o tubo (opcional)
-      // tubo.style.top = `${Math.floor(Math.random() * (window.innerHeight - 300) + 100)}px`;
     }
   });
 }
@@ -33,25 +30,25 @@ const loop = setInterval(() => {
     const tuboPosition = tubo.offsetLeft;
     const brendaPosition = +window.getComputedStyle(brenda).bottom.replace('px', '');
     
-    // Verifica colisão
+    // verifica colisão
     if (tuboPosition <= 120 && tuboPosition > 0 && brendaPosition < 80) {
-      // Quando a Brenda colide, pausa todos os tubos
+      // quando a Brenda colide, os tubos param
       tubos.forEach(t => {
         t.style.animationPlayState = 'paused';
-        t.style.left = `${t.offsetLeft}px`; // Mantém o tubo na mesma posição
+        t.style.left = `${t.offsetLeft}px`; //tubo na mesma posição
       });
       
       brenda.style.animation = 'none'; 
       brenda.style.bottom = `${brendaPosition}px`; 
       brenda.src = 'img/brenda_morta-1.png-removebg-preview.png'; 
-      brenda.style.width = '150px'; 
+      brenda.style.width = '196px'; 
       brenda.style.transition = 'none'
       brenda.style.position = 'absolute'; 
 
       clearInterval(loop);
     }
 
-    reposicionarTubos(); // Reposiciona os tubos
+    reposicionarTubos();
   });
 }, 10);
 
